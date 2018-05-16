f2829277c9cd5ef6154174cea558365bfb66dda5
Jean Augusto Silva <jeanaugustoos@gmail.com>
2018-05-16 14:38:28 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const moment = require('moment')
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  const isSuccess = moment().minute() % 2 === 0
+
+  const responseStatusCode = isSuccess ? 200 : 500
+  res.sendStatus(responseStatusCode)
+})
+
+app.post('/', (req, res) => {
+  const isSuccess = moment().minute() % 2 === 0
+
+  const responseStatusCode = isSuccess ? 200 : 500
+  res.status(responseStatusCode).send('<foo>123</foo>')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
